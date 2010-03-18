eb85f0667c32f78ee023837fa0eed7e61df5d474
Walter Georgescu <walter.georgescu@vanderbilt.edu>
2010-03-18 13:36:08 -0500

    Modified reconstructObjects so the guide image always has less pixels than the image we reconstruct from.

--- reconstructObjects.m
+++ reconstructObjects.m
@@ -1,6 +1,7 @@
 function output_args=reconstructObjects(input_args)
 
-output_args.Image=imreconstruct(input_args.GuideImage.Value,input_args.ImageToReconstruct.Value);
+img_and=input_args.GuideImage.Value&input_args.ImageToReconstruct.Value;
+output_args.Image=imreconstruct(img_and,input_args.ImageToReconstruct.Value);
 
 %end reconstructObjects
 end
