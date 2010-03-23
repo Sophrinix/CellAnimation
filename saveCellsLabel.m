d48fafd3765c2bf9ade6c6980d05b115529e25cf
Walter Georgescu <walter.georgescu@vanderbilt.edu>
2010-03-23 12:21:10 -0500

    Fixed bug where previous cell label was not being updated and was instead always the first label.

--- saveCellsLabel.m
+++ saveCellsLabel.m
@@ -2,7 +2,7 @@ function output_args=saveCellsLabel(input_args)
 
 cells_lbl=input_args.CellsLabel.Value;
 save([input_args.FileRoot.Value num2str(input_args.CurFrame.Value,input_args.NumberFormat.Value)],'cells_lbl');
-output_args=[];
+output_args.CellsLabel=cells_lbl;
 
 %end saveCellsLabel
 end
