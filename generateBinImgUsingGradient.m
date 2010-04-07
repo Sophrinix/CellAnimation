19b7fe68d981d9a2b04debbdf1284108393a0dd1
Walter Georgescu <walter.georgescu@vanderbilt.edu>
2010-04-07 13:55:34 -0500

    Added local thresholding algorithm that uses the image gradient as the local property.

---
+++ generateBinImgUsingGradient.m
@@ -0,0 +1,18 @@
+function output_args=generateBinImgUsingGradient(input_args)
+[grad_x grad_y]=gradient(double(input_args.Image.Value));
+grad_mag=sqrt(grad_x.^2+grad_y.^2);
+img_bw=grad_mag>input_args.GradientThreshold.Value;
+clear_border_dist=input_args.ClearBorderDist.Value;
+if (input_args.ClearBorder.Value)
+    if (clear_border_dist>1)
+        img_bw(1:clear_border_dist-1,1:end)=1;
+        img_bw(end-clear_border_dist+1:end,1:end)=1;
+        img_bw(1:end,1:clear_border_dist-1)=1;
+        img_bw(1:end,end-clear_border_dist+1:end)=1;
+    end
+    output_args.Image=imclearborder(img_bw);
+else
+    output_args.Image=img_bw;
+end
+
+end %end generateBinImgUsingGradient
