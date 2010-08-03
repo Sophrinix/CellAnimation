6d95818faa4305cf891e3f460587728b1b87db91
Walter Georgescu <walter.georgescu@vanderbilt.edu>
2010-08-03 18:03:45 -0500

    Modified function to use selection layers.

--- checkBoxLabelsEvent.m
+++ checkBoxLabelsEvent.m
@@ -2,9 +2,7 @@ function checkBoxLabelsEvent()
 
 global mtr_gui_struct;
 mtr_gui_struct.ShowLabels=get(mtr_gui_struct.CheckBoxLabelsHandle,'Value');
-updateTrackImage(mtr_gui_struct.CurFrame,mtr_gui_struct.ShowLabels);
-if (mtr_gui_struct.SelectedCellID>0)
-    selectCell(mtr_gui_struct.SelectedCellLabelID);    
-end
+updateTrackImage(mtr_gui_struct.CurFrame,mtr_gui_struct.ShowLabels,mtr_gui_struct.ShowOutlines);
+addSelectionLayers();
 
 end
