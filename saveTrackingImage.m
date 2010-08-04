5a9390bf30bda7e93df5b4e8cd12e45626e6bf9e
Walter Georgescu <walter.georgescu@vanderbilt.edu>
2010-08-04 12:53:37 -0500

    Added option to save tracking image as a 300 dpi tiff file

--- saveTrackingImage.m
+++ saveTrackingImage.m
@@ -1,8 +1,8 @@
 function saveTrackingImage()
 global mtr_gui_struct;
 
-[file_name path_name]=uiputfile('*.jpg','Save Frame Image');
-if (~file_name)
+[file_name path_name filter_index]=uiputfile({'*.jpg';'*.tif'},'Save Frame Image');
+if (~filter_index)
     return;
 end
 hidden_figure_handle=figure('visible','off');
@@ -11,7 +11,12 @@ set(hidden_figure_handle,'PaperPositionMode','auto');
 subplot('Position', [0 0 1 1]);
 hidden_axes=copyobj(mtr_gui_struct.TracksHandle,hidden_figure_handle);
 set(hidden_axes, 'units', 'normalized', 'position', [0 0 1 1]);
-print(hidden_figure_handle, '-djpeg', [path_name file_name])
+switch (filter_index)
+    case 1
+        print(hidden_figure_handle, '-djpeg', [path_name file_name]);
+    case 2
+        print(hidden_figure_handle, '-dtiff', '-r300', [path_name file_name]);
+end
 
 %end saveImage
 end
