3d9235a9d26eca4a25cd847965d150355211be2b
Walter Georgescu <walter.georgescu@vanderbilt.edu>
2010-12-22 15:31:23 -0600

    Removed obsolete files from CA directory.

--- full_track_nuclei.m
+++
@@ -1,7 +0,0 @@
-function []=full_track_nuclei(img_filebase,seg_file_root,track_dir,tracks_file,prol_file_root,prol_dir,...
-    prol_xls_file,shapes_xls_file,startframe,framecount,timeframe, ds, img_ext)
-darren_track(img_filebase,seg_file_root,track_dir,startframe,framecount,timeframe, ds, img_ext);
-extract_movie_data(img_filebase,seg_file_root,prol_file_root,prol_dir,tracks_file,startframe,framecount,...
-    timeframe,shapes_xls_file, ds, img_ext);
-displayprolstats(prol_dir,prol_xls_file, ds);
-end
--- is_empty_function.m
+++
@@ -1,6 +0,0 @@
-function output_args=is_empty_function(input_args)
-
-output_args.Boolean=isempty(input_args.TestVariable.Value);
-
-%end is_empty_function
-end
