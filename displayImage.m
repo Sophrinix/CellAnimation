780f486e97ef72d55d2d99e70907e4f090ff3a8a
Walter Georgescu <walter.georgescu@vanderbilt.edu>
2011-03-14 15:46:03 -0500

    Modified display image so it sets the intensity limits to the image limits.

--- displayImage.m
+++ displayImage.m
@@ -2,7 +2,7 @@ function output_args=displayImage(input_args)
 %displayImage module
 %create or select figure FigureNr and display the image provided in Image
 
-showmaxfigure(input_args.FigureNr.Value), imshow(input_args.Image.Value);
+showmaxfigure(input_args.FigureNr.Value), imshow(input_args.Image.Value,[]);
 output_args=[];
 
 end
