3807a78a2039154e4b0342e0f9d66e4aec7c8ef8
Walter Georgescu <walter.georgescu@vanderbilt.edu>
2011-07-22 14:27:02 -0500

    Changed named of the output argument in imageErode.

--- imageErode.m
+++ imageErode.m
@@ -2,7 +2,7 @@ function output_args=imageErode(input_args)
 %module shell for the Matlab imerode(im,se) function
 img=input_args.Image.Value;
 se=input_args.StructuralElement.Value;
-output_args.ErodedImage=imerode(img,se);
+output_args.Image=imerode(img,se);
 
 %end imageErode
 end
