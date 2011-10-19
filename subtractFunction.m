684ba3f5fe6eb00c56334d5d7988eb7c77d030e7
Walter Georgescu <walter.georgescu@vanderbilt.edu>
2011-10-18 22:57:17 -0500

    Added new module subtractFunction.m

---
+++ subtractFunction.m
@@ -0,0 +1,18 @@
+function output_args=subtractFunction(input_args)
+%Usage
+%This module subtracts one variable from another.
+%
+%Input Structure Members
+%Number1 - The first variable.
+%Number2 - The variable to be subtracted.
+%
+%Output Structure Members
+%Difference - The result of the addition.
+
+
+arg_1=input_args.Number1.Value;
+arg_2=input_args.Number2.Value;
+output_args.Difference=(arg_1-arg_2);
+
+%end addFunction
+end
