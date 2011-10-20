d96e021be1dd4eb361ce82e5e7174d2597c564f7
Walter Georgescu <walter.georgescu@vanderbilt.edu>
2011-10-20 01:44:50 -0500

    Removed some functions not ready for prime time.

--- startTracksNoShapeProps.m
+++
@@ -1,9 +0,0 @@
-function output_args=startTracksNoShapeProps(input_args)
-%these are simple tracks - no shape properties
-object_centroids=input_args.ObjectCentroids.Value;
-cur_time=repmat((input_args.CurFrame.Value-1)*input_args.TimeFrame.Value,size(object_centroids,1),1);
-track_ids=[1:size(object_centroids,1)]';
-output_args.Tracks=[track_ids cur_time object_centroids];
-
-%end startTracks
-end
